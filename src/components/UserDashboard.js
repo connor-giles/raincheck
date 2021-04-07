f75e3eb94aee4994477212f1881a1793937ab858
Connor Giles <39887479+connor-giles@users.noreply.github.com>
2021-04-07 00:21:33 -0400

    added profile weather functionality

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -1,15 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Card, Button, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext.js'
 import { Link, useHistory } from 'react-router-dom'
 import helperFunctions from '../firebase.js'
 
+const weatherAPI = {
+    key: "2a57815a865fa327116a8e960e80aa9e",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
 export default function UserDashboard() {
     
     const[error, setError] = useState('')
     const[homeT, setHomeT] = useState('')
+    const [weather, setWeather] = useState({});
     const { currentUser, logout } = useAuth()
     const history = useHistory()
+
+    //builds the date info for the user dashboard
+    const dateBuilder = (d) => {
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+
+        return `${day} ${date} ${month} ${year}`
+    }
 
     async function handleLogout() {
         setError('')
@@ -28,8 +47,17 @@ export default function UserDashboard() {
     .then((res) => {
         setHomeT(res.homeTown)
     })
-    
-    
+
+    //gets weather based on the users hometown and displays it on profile
+    useEffect(() => {
+        fetch(`${weatherAPI.base}weather?q=${homeT}&units=metric&APPID=${weatherAPI.key}`)
+        .then(res => res.json())
+        .then(result => {
+        setWeather(result);
+        console.log(result)
+        });
+      }, [homeT]);
+
     
     return (
         <>
@@ -38,13 +66,28 @@ export default function UserDashboard() {
                     <h2 className="text-center mb-4">Profile</h2>
                     {error && <Alert variant="danger">{error}</Alert>}
                     <strong>Email: </strong> {currentUser.email}
+                    <br></br>
                     <strong>Hometown: </strong> {homeT}
                     <Link to="/update-profile" className="btn btn-primary w-100 mt-3">Edit Profile</Link>
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
                 <Button variant="link" onClick={handleLogout}>Log Out</Button>
-            </div> 
+            </div>
+
+            {(typeof weather.main != "undefined") ? (
+                <div>
+                <div className="location-box">
+                    <div className="location">{weather.name}, {weather.sys.country} </div>
+                    <div className="date">{dateBuilder(new Date())}</div>
+                </div>
+
+                <div className="weather-box">
+                    <div className="temp">{Math.round(weather.main.temp * 9 / 5 + 32)}°F</div>
+                    <div className="weather">{weather.weather[0].main}</div>
+                </div>
+                </div>
+            ) : ('')}
         </>
     )
 }
