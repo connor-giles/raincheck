dd9f80962bdaa8cdc14923f099dde87ecb943c51
Connor Giles <39887479+connor-giles@users.noreply.github.com>
2021-04-20 20:29:21 -0400

    updated frontend for add event page

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -1,26 +1,58 @@
 import React, { useState } from 'react';
-import TextField from '@material-ui/core/TextField';
 import { DatePicker, Space } from 'antd';
 import 'antd/dist/antd.css';
 import { makeStyles } from '@material-ui/core/styles';
-import { Button, Checkbox, FormControlLabel } from '@material-ui/core';
+import { Button, Checkbox, FormControlLabel, Grid, Typography } from '@material-ui/core';
+import { TextField, Container, CssBaseline, Avatar, Paper, Box } from '@material-ui/core';
 import firebase from 'firebase/app'
 import helperFunctions from '../firebase.js'
 import { useHistory } from "react-router-dom"
+import TodayIcon from '@material-ui/icons/Today';
+import Image from '../assets/pic1.jpg';
 
 
-const useStyles = makeStyles({
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: '10%',
+
+const useStyles = makeStyles((theme) => ({
+    fullPage: {
+        backgroundImage:  `url(${Image})`
     },
-    title: {
-        marginBottom: '6%'
-    }
-})
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'block',
+      flexDirection: 'column',
+      padding: 50,
+      height: '45vh',
+      width: 600,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: '#3b5785'
+    },
+    datePicker: {
+      marginTop: '20px'
+    },
+    outdoor: {
+      paddingTop: 30
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submitButton: {
+      margin: theme.spacing(3, 0, 2),
+      maxWidth: '200px',
+      marginRight: '15px',
+      backgroundColor: '#3b5785'
+    },
+    cancelButton: {
+      margin: theme.spacing(3, 0, 2),
+      maxWidth: '200px',
+      marginLeft: '15px',
+      backgroundColor: '#3b5785'
+    },
+}));
 
 export default function AddEvent(props) {
     const classes = useStyles() //handles css stuff
@@ -73,50 +105,82 @@ export default function AddEvent(props) {
     }
     
     return(
-        <div className={classes.container}>
+        <div className={classes.fullPage}>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box display="flex" alignItems="center" justifyContent="center">
+                <Paper elevation={10} className={classes.paper} square={false} varient='outlined' align='center'>
+                    <Avatar className={classes.avatar} align="center">
+                        <TodayIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5" align='center'>
+                        Add New Event
+                    </Typography>
+                    <form className={classes.form} noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="Event Name"
+                            name="eventName"
+                            onChange={(e) => setEventName(e.target.value)}
+                            />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="eventLoc"
+                            label="Event Location (City Name)"
+                            onChange={(e) => setEventLocation(e.target.value)}
+                        />
 
-        <h1 className={classes.title}>Add Event Title Below</h1>
-        <form noValidate autoComplete="off">
-            <TextField onChange={(e) => setEventName(e.target.value)} id="filled-basic" label="Enter Event Name" variant="filled" color="secondary"/>
-        </form>
+                        <div className={classes.datePicker}>
+                            <Space direction="vertical" size={12}>
+                                <DatePicker showTime onChange={onChange} style={{ width: 500, height: 50}}/>
+                            </Space>
+                        </div>
 
-
-        <h1 className={classes.title}>Add Event Infomation Below</h1>
-        <div>
-            <Space direction="vertical" size={12}>
-                <DatePicker showTime onChange={onChange} />
-            </Space>
+                        <FormControlLabel className = {classes.outdoor}
+                            control={
+                                <Checkbox
+                                    checked={isOutdoors}
+                                    onChange={toggle}
+                                    name="indoorOutdoor"
+                                    color="primary"
+                                />
+                            }
+                            label="Will your event take place outdoors?"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submitButton}
+                            onClick={() => {
+                                sendInfoToDb()
+                                history.push("/userdashboard")
+                            }}>
+                            Submit Event Info
+                        </Button>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.cancelButton}
+                            onClick={() => {
+                                history.push("/userdashboard")
+                            }}>
+                            Cancel
+                        </Button>
+                    </form>
+                </Paper>
+            </Box>
+        </Container>
         </div>
-
-        <h1 className={classes.title}>Event Location</h1>
-        <form noValidate autoComplete="off">
-            <TextField onChange={(e) => setEventLocation(e.target.value)} id="filled-basic" label="Enter Event Location (City)" variant="filled" color="secondary"/>
-        </form>
-        <FormControlLabel
-            control={
-                <Checkbox
-                    checked={isOutdoors}
-                    onChange={toggle}
-                    name="indoorOutdoor"
-                    color="primary"
-                />
-            }
-            label="Outdoors"
-        />
-
-        <Button variant="outlined" color="primary"
-        onClick={() => {
-            sendInfoToDb()
-            history.push("/userdashboard")
-        }}>
-            Submit Event Info
-        </Button>
-
-        <Button variant="outlined" color="primary" onClick={() => {history.push("/userdashboard") }}>
-            Cancel
-        </Button>
-
-        </div>
-    )
-
+    );
+    
 }
