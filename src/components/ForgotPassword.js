f38a7f4b1c4179bd7e62128c57d7cb9c74be6af0
Connor Giles <39887479+connor-giles@users.noreply.github.com>
2021-04-21 03:35:12 -0400

    added reset password page

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -2,6 +2,20 @@ import React, {useRef, useState } from 'react'
 import { Card, Form, Button, Container, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext.js'
 import { Link } from "react-router-dom"
+import { makeStyles } from '@material-ui/core/styles';
+import Image from '../assets/pic3resize.jpg';
+import { Box, Avatar } from '@material-ui/core';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+
+const useStyles = makeStyles((theme) => ({
+    fullPage: {
+        backgroundImage:  `url(${Image})`
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: '#3b5480',
+    },
+}));
 
 export default function ForgotPassword() {
 
@@ -10,6 +24,7 @@ export default function ForgotPassword() {
     const[error, setError] = useState('')
     const[message, setMessage] = useState('')
     const[loading, setLoading] = useState(false)
+    const classes = useStyles() //handles css stuff
 
     //asynchronous functions that handles the submission of the user's account
     async function handleSubmit(e) {
@@ -30,11 +45,16 @@ export default function ForgotPassword() {
     }
 
     return (
-        <>
+        <div className={classes.fullPage}>
            <Container className="d-flex align-items center justify-content-center pt-5" style={{ minHeight: "100vh "}}>
                 <div className="w-100" style={{ maxWidth: "400px" }}>
                     <Card>
                         <Card.Body>
+                            <Box display="flex" alignItems="center" justifyContent="center">
+                                <Avatar className={classes.avatar} align="center">
+                                    <HelpOutlineIcon />
+                                </Avatar>
+                            </Box>
                             <h2 className="text-center mb-4">Password Reset</h2>
                             {error && <Alert variant="danger">{error}</Alert>}
                             {message && <Alert variant="success">{message}</Alert>}
@@ -43,20 +63,20 @@ export default function ForgotPassword() {
                                         <Form.Label>Email</Form.Label>
                                         <Form.Control type="email" ref={emailRef} required />
                                     </Form.Group>
-                                    <Button disabled={loading} className="w-100"type="submit">
+                                    <Button disabled={loading} style={{backgroundColor: '#3b5480'}} className="w-100"type="submit">
                                         Reset Password
                                     </Button>
                                 </Form>
                                 <div className="w-100 text-center mt-3">
-                                    <Link to="/login">Login</Link>
+                                    <Link to="/login" style={{color: '#3b5480'}}>Return to Login</Link>
                                 </div>
                         </Card.Body>
                     </Card>
                     <div className="w-100 text-center mt-2">
-                            Dont have an account? <Link to="/signup">Sign Up</Link>
+                            Dont have an account? <Link to="/signup" style={{color: '#3b5480'}}>Sign Up</Link>
                     </div> 
                 </div>
            </Container>
-        </>
+        </div>
     )
 }
